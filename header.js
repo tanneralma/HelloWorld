692538b559458df10cb085882f6bf912eb431ff4
Animcogn <corbanvilla@gmail.com>
2019-01-08 22:25:05 -0700

    added footer

--- header.js
+++ header.js
@@ -1,17 +1,21 @@
-// Sticky Header
-window.onscroll = function() {stickyHeader()};
+// Dont't load variable until after page loads
+$(window).bind("load", function() {
+    // Sticky Header
+    window.onscroll = function() {stickyHeader()};
 
-var topHeader = document.getElementById("header");
+    var topHeader = document.getElementById("header");
 
-var sticky = topHeader.offsetTop;
+    var sticky = topHeader.offsetTop;
 
-function stickyHeader() {
+    function stickyHeader() {
 
-    console.log(topHeader);
-
-    if (window.pageYOffset > sticky) {
-        topHeader.classList.add("sticky");
-    } else {
-        topHeader.classList.remove("sticky");
+        console.log(topHeader);
+    
+        if (window.pageYOffset > sticky) {
+            topHeader.classList.add("sticky");
+        } else {
+            topHeader.classList.remove("sticky");
+        }
     }
-}
+
+});
